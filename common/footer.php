5630230e105d6f8da5960d6109d2488bb590e1ca
GShacklebolt <ghazali123456@gmail.com>
2020-11-21 23:32:14 +0530

    Removed document.write() from footer

--- common/footer.php
+++ common/footer.php
@@ -4,9 +4,10 @@
         </span>
       </div>
       <div class="copyright">
-		  <p>Designed &amp; Developed by <a href="https://ghazali.ml/" onMouseOver="this.style.color='#800080'" onMouseOut="this.style.color='#1688f0'">Ghazali</a> aka GShacklebolt
-      </p>
-      <p> Copyright &copy; <script>document.write(new Date().getFullYear());</script> | All Rights Reserved
-      </p>
+        <p id="copyright">
+          <script>document.getElementById("copyright").innerHTML="Copyright &copy; "+new Date().getFullYear()+" | All Rights Reserved";</script>
+        </p>
+  		  <p>Designed &amp; Developed by <a href="https://ghazali.ml/" onMouseOver="this.style.color='#800080'" onMouseOut="this.style.color='#1688f0'">Ghazali</a>
+        </p>
       </div>
     
