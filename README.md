876520a2fcf37683238ca85d4699946bfb06c36e
GShacklebolt <ghazali123456@gmail.com>
2022-04-17 21:16:07 +0530

    Refreshed

--- README.md
+++
@@ -1,9 +0,0 @@
-# Ghazali Shacklebolt
-##### Published Site : [www.ghazali.ml](https://www.ghazali.ml/)
-
-***
-
-Ghazali.ml is a personal site of Ghazali created for the purpose of serving as an online portfolio. 
-To find more about me, my latest projects or just to get in touch with me checkout this website.
-
-gshacklebolt.github.io redirects to [www.ghazali.ml](https://www.ghazali.ml/)
--- common/footer.php
+++ common/footer.php
@@ -7,10 +7,10 @@
         <p id="copyright">
           <script>document.getElementById("copyright").innerHTML="Copyright &copy; "+new Date().getFullYear()+" | All Rights Reserved";</script>
         </p>
-  		  <p>Designed &amp; Developed by <a href="https://www.ghazali.ml/" onMouseOver="this.style.color='#800080'" onMouseOut="this.style.color='#1688f0'">Ghazali</a>
+  		  <p>Designed &amp; Developed by <a href="https://www.ghazali.ga/" onMouseOver="this.style.color='#800080'" onMouseOut="this.style.color='#1688f0'">Ghazali</a>
         </p>
         <p class="hiddenText">
-          Also checkout <a href="https://www.kasif.ml/">Md Kasif's</a> site
+          Also checkout <a href="https://www.kasif.tech/">Md Kasif's</a> site
         </p>
       </div>
     
