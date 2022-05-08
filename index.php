9562cac3201670ac9eb472208d3b6caff5ce22f0
GShacklebolt <ghazali123456@gmail.com>
2022-05-08 20:20:58 +0530

    Adding index.php

---
+++ index.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Ghazali.ga is a personal site of Ghazali created to serve as an online portfolio. To get in touch, to find more about me or my projects checkout my website." />
+    <meta name="keywords" content="Ghazali, Ghazali Shacklebolt, GShacklebolt, ghazali.ga, Ghazali's portfolio, quedicate.ml, ghazali123456, Shacklebolt, Haza, Dilawarpur, Munger, ndamunger, notre dame, Bhopal, Corporate Institute of Science and Technology, CIST, corporatebpl, Wipro, Best Software Developer, Web Developer, Web Designer, Digital Marketing Expert, Content Creator">
+    <meta name="author" content="Ghazali">
+
+    <!--Verify on Bing Webmaster-->
+    <meta name="msvalidate.01" content="9BC2739931AB1893001315FE381B6148" />
+    
+    <title>Ghazali Shacklebolt | Home</title>
+
+    <link rel="canonical" href="https://www.ghazali.ga/" />
+
+    <link rel="icon" type="image/ico" href="./images/ghazali.ico" />
+
+    <!-- For Performance Improvements -->  
+    <link
+      crossorigin
+      rel="preload"
+      href="https://fonts.googleapis.com/css?family=Great+Vibes|Lato:300,400,700,900&display=swap"
+      as="style"
+      onload="this.onload=null;this.rel='stylesheet'"
+    />
+    <link
+      crossorigin="anonymous"
+      integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw=="
+      rel="preload"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
+      as="style"
+      onload="this.onload=null;this.rel='stylesheet'"
+    />
+    <link
+      crossorigin
+      rel="preload"
+      href="https://cdnjs.cloudflare.com/ajax/libs/devicon/2.4/devicon.min.css"
+      as="style"
+      onload="this.onload=null;this.rel='stylesheet'"
+    />
+	  
+    <link rel="preload" href="./styles/bootstrap.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+
+    <link href="./styles/styles.min.css" rel="stylesheet" />
+
+    <noscript>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Great+Vibes|Lato:300,400,700,900&display=swap" />
+      <link
+        crossorigin="anonymous"
+        integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw=="
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
+      />
+		
+      <link crossorigin rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/devicon/2.4/devicon.min.css" />
+      
+      <link href="./styles/bootstrap.min.css" rel="stylesheet" />
+      
+		<style>
+        #heading {
+          padding-bottom: 20px;
+        }
+        #subheading {
+          display: none;
+        }
+      </style>
+    </noscript>
+    <style>
+        .disclaimer {
+            display: none! important;
+        }
+    </style>
+  </head>
+
+  <body>
+    <div id="mobile-menu-open" class="shadow-large">
+      <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <nav id="myHeaderNav">
+      <header accesskey="2" tabindex="0">
+        <div id="mobile-menu-close">
+          <i class="fa fa-window-close shadow-large" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+          <li>
+            <a href="#lead">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+        </ul>
+      </header>
+    </nav>
+    <!-- End header -->
+
+    <section id="lead">
+      <div id="lead-content">
+        <img id="me" src="./images/ghazali.jpg" alt="Ghazali Shacklebolt" />
+        <h2>______________________________________</h2>
+        <h1 id="heading">Ghazali<span class="hiddenText"> Shacklebolt's Personal Website</span></h1>
+        <h2 id="subheading">
+          I'm
+			<span class="typewriter"
+            data-period="2000"
+            data-type='["a passionate Programmer.", "elated to see you here ;)","a Bibliophile.","a Tech-Savant.", "a Reader.", "a Learner.", "an Explorer.", "a Problem Solver.", "a Potterhead.","in a Permanent Beta :P",  "hoping you send me a Hi! :)" ]'>
+            <span class="wrap"></span>
+          </span>
+        </h2>
+        <nav class="social">
+          <ul>
+            <li>
+              <a href="mailto:ghazali123456@gmail.com" target="_blank" rel="noopener" accesskey="m">
+                <i class="fas fa-envelope" aria-hidden="true" title="Ghazali's Mail"></i>
+              </a>
+            </li>
+            <!--<li>
+              <a href="https://gshacklebolt.blogspot.com" target="_blank" rel="noopener" accesskey="b">
+                <i class="fab fa-blogger" aria-hidden="true" title="Ghazali's Blog"></i>
+              </a>
+            </li>-->
+            <li>
+              <a href="https://github.com/gshacklebolt" target="_blank" rel="noopener" accesskey="g">
+                <i class="fab fa-github" aria-hidden="true" title="Ghazali's GitHub Profile"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com/in/gshacklebolt" target="_blank" rel="noopener" accesskey="l">
+                <i class="fab fa-linkedin" aria-hidden="true" title="Ghazali's LinkedIn Profile"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://facebook.com/gshacklebolt" target="_blank" rel="noopener" accesskey="f">
+                <i class="fab fa-facebook" aria-hidden="true" title="Ghazali's Facebook Profile"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/gshacklebolt" target="_blank" rel="noopener" accesskey="t">
+                <i class="fab fa-twitter" aria-hidden="true" title="Ghazali's Twitter Profile"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://instagram.com/gshacklebolt" target="_blank" rel="noopener" accesskey="i">
+                <i class="fab fa-instagram" aria-hidden="true" title="Ghazali's Instagram Profile"></i
+              ></a>
+            </li>
+            <!--<li>
+              <a
+                href=""
+                target="_blank"
+                rel="noopener"
+                accesskey="y"
+              >
+                <i class="fab fa-youtube" aria-hidden="true" title="Ghazali's Youtube Channel"></i
+              ></a>
+            </li>-->
+            <!--<li>
+              <a href="https://dev.to/gshacklebolt" target="_blank" rel="noopener" accesskey="d">
+                <i class="fab fa-dev" aria-hidden="true" title="Ghazali's Dev Profile"></i>
+              </a>
+            </li>-->
+            <li>
+              <a href="https://hackerrank.com/gshacklebolt" target="_blank" rel="noopener" accesskey="h">
+                <i class="fab fa-hackerrank" aria-hidden="true" title="Ghazali's HackerRank Profile"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://t.me/gshacklebolt" target="_blank" rel="noopener" accesskey="z">
+                <i class="fab fa-telegram-plane" aria-hidden="true" title="Ghazali's Telegram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="tel:+91 7033739442"rel="noopener" accesskey="p">
+                <i class="fas fa-phone-alt" aria-hidden="true" title="Call Ghazali"></i
+              ></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <!-- End #lead-content -->
+
+      <div id="lead-overlay"></div>
+
+      <div id="lead-down">
+        <span>
+          <i class="fa fa-chevron-down" aria-hidden="true" title="More"></i>
+        </span>
+      </div>
+      <!-- End #lead-down -->
+    </section>
+    <!-- End #lead -->
+
+    <section id="about" accesskey="a">
+      <h2 class="heading">About Me</h2>
+      <p>
+        Hi visitor, Ghazali (aka GShacklebolt) here. Welcome to my little space on the web.
+        <br /><br />
+        I am a passionate software developer and a problem solver in permanent beta.
+        Learning new things, making stuff and exploring technology are my passion. <br />
+        I have experience in web development using HTML, PHP, CSS, Bootstrap, Javascript, AJAX, jQuery, MySQL, Oracle DB. <br />I did my minor project using Java. My current language of choice is Java.<br />
+        Currently, I am working for Wipro Limited.
+        <br/ ><br />
+        Also, I love reading novels, mostly fiction.
+        <br /><br />
+        You can find links to my <a href="https://github.com/gshacklebolt" target="_blank" rel="noopener">GitHub</a>, <a href="https://twitter.com/gshacklebolt" target="_blank" rel="noopener">Twitter</a> and other social profiles and various projects here. Feel free to
+        <a href="mailto:ghazali123456@gmail.com" target="_blank" rel="noopener">get in touch</a>
+        if anything here managed to catch your attention.<br /><br />
+
+        <a class="btn-rounded-blue" style="margin-top: 20px;" rel="noopener" target="_blank" href="https://drive.google.com/file/d/1IwgZotPUQVn04Rs73SB1lDZzyjcKzunE/view?usp=sharing">Download Resume</a>
+        <a class="btn-rounded-blue" style="margin-top: 20px;" rel="noopener" target="_blank" onclick="window.print()">Print this site</a>
+      </p>
+    </section>
+    <!-- End #about -->
+
+    <!-- Adverts -->
+    <?php
+      //include("./adverts.php");
+    ?>
+    <!-- Adverts -->
+
+    <section id="experience" class="background-alt" accesskey="e">
+      <h2 class="heading">Experience</h2>
+      <div id="experience-timeline">
+        <div data-date="March 2021 – Present" class="shadow">
+          <a href="https://www.wipro.com/" target="_blank">
+          	<h3>Wipro Limited <img src="./images/company/wipro.webp" alt="Wipro Logo" class="company-logo" /></h3>
+          </a>
+          <h4>Project Engineer</h4>
+          <h5>ServiceNow Developer using Angular, HTML, CSS</h5>
+          <h5>Certifications - ServiceNow CSA, CSM, Custom App Development</h5>
+          <ul>
+            <!-- <li></li> -->
+          </ul>
+        </div>
+        <div data-date="July 2019 – January 2020" class="shadow">
+          <a href="http://fidaato.in/" target="_blank">
+          	<h3>Fidaato Infotech Pvt Ltd <img src="./images/company/fidaato.webp" alt="Fidaato Logo" class="company-logo" /></h3>
+          </a>
+          <h4>Web Developer Intern</h4>
+          <h5>PHP, HTML, CSS, Bootstrap, JavaScript, AJAX, MySQL </h5>
+          <ul>
+            <!-- <li></li> -->
+          </ul>
+        </div>
+        <div data-date="June 2019 – July 2019" class="shadow">
+          <a href="http://parabit.in/" target="_blank">
+            <h3>Parabit Technology Pvt. Ltd <img src="./images/company/parabit.webp" alt="Parabit Logo" class="company-logo" /></h3>
+          </a>
+          <h4>Software Development Intern</h4>
+          <h5>Java & MySQL in NetBeans</h5>
+          <ul>
+            <!-- <li></li> -->
+          </ul>
+        </div>
+      </div>
+    </section>
+    <!-- End #experience -->
+
+    <section id="education" accesskey="r">
+      <h2 class="heading">Education</h2>
+      <div class="education-block shadow">
+        <a href="https://www.corporatebpl.com/" target="_blank">
+        	<h3>Corporate Institute of Science &amp; Technology, Bhopal</h3>
+        </a>
+        <span class="education-date"> 2016 - 2020</span>
+        <h4>Bachelor of Engineering (B.E.) in Computer Science &amp; Engineering</h4>
+        <ul>
+          <li>Majored in Computer Science &amp; Engineering.</li>
+          <li>Took various CS courses, both theoretical and practical.</li>
+        </ul>
+      </div>
+      <!-- End .education-block -->
+      <div class="education-block shadow">
+        <h3>Krishnanand Memorial Academy, Munger</h3>
+        <span class="education-date"> 2013 - 2016</span>
+        <h4>Higher Secondary Education (12th)</h4>
+        <ul>
+          <li>Graduated Higher Secondary School with Science (Mathematics).</li>
+        </ul>
+      </div>
+      <!-- End .education-block -->
+      <div class="education-block shadow">
+        <a href="http://www.ndmunger.com/" target="_blank">
+        	<h3>Notre Dame Academy, Munger</h3>
+        </a>
+        <span class="education-date"> 2011 - 2013</span>
+        <h4>Secondary Education (10th)</h4>
+        <ul>
+          <li>Graduated Secondary School with Foundation of IT as optional.</li>
+        </ul>
+      </div>
+      <!-- End .education-block -->
+    </section>
+    <!-- End #education -->
+
+    <section id="projects" class="background-alt" accesskey="p">
+      <h2 class="heading">Projects</h2>
+      <div class="projects-grid">
+		  
+        <div class="project shadow">
+          <div class="project-info">
+            <h3>PSCIS</h3>
+            <p>Digitization and Centralization of Police System.</p>
+            <a href="" target="_blank" rel="noopener" class="btn-rounded-blue">View Project</a>
+          </div>
+        </div>
+		  
+        <div class="project shadow">
+          <div class="project-info">
+            <h3>Quedicate</h3>
+            <p>E-Platform ticketing website</p>
+            <a href="https://github.com/gshacklebolt/quedicate" target="_blank" rel="noopener" class="btn-rounded-blue">View Project</a>
+            <a href="https://www.ghazali.ga/projects/quedicate" target="_blank" rel="noopener" class="btn-rounded-blue">View Live Site</a>
+          </div>
+        </div>
+		  
+      </div>
+      <p>
+        <a href="https://github.com/gshacklebolt?tab=repositories" target="_blank" rel="noopener" class="btn-rounded-blue">More Projects</a>
+      </p>
+    </section>
+
+    <!-- End #projects -->
+
+    <section id="skills" accesskey="s">
+      <h2 class="heading">Skills</h2>
+      <ul>
+        <li><i class="devicon-angularjs-plain colored"></i> Angular</li>  
+        <li><i class="devicon-java-plain colored"></i> Java</li>
+        <li><i class="devicon-php-plain colored"></i> PHP</li>
+        <li><i class="devicon-html5-plain colored"></i> HTML</li>
+        <li><i class="devicon-css3-plain colored"></i> CSS</li>
+        <li><i class="devicon-javascript-plain colored"></i> JavaScript</li>
+        <li><i class="devicon-typescript-plain colored"></i> TypeScript</li>
+        <li><i class="devicon-bootstrap-plain colored"></i> Bootstrap</li>
+        <li><i class="devicon-jquery-plain colored"></i> jQuery</li>
+        <li><i class="devicon-mysql-plain colored"></i> MySQL</li>
+        <li><i class="devicon-oracle-plain colored"></i> Oracle DB</li>
+        <li><i class="devicon-python-plain colored" style="color: #3472a6;"></i> Python</li>
+        <li><i class="devicon-c-plain colored"></i> C</li>
+        <li><i class="devicon-cplusplus-plain colored"></i> C++</li>
+        <li><i class="devicon-git-plain colored"></i> Git</li>
+        <li><i class="fab fa-markdown"></i> Markdown</li>
+        <li><i class="fab fa-linux"></i> Linux</li>
+        <li>( .* ) Regex</li>
+        <!--<li><i class="fa fa-terminal"></i> Bash</li>-->
+        <!--<li><i class="fab fa-kickstarter-k" style="color: #796bdc;"></i> Kotlin</li>-->
+        <!--<li><i class="devicon-ruby-plain colored"></i> Ruby</li>-->
+        <!--<li><i class="devicon-go-plain colored" style="color: #2ad2e4;"></i> Go</li>-->
+        <!--<li><i class="devicon-rust-plain colored" style="color: #888786;"></i> Rust</li>-->
+        <!--<li><i class="fab fa-r-project" style="color: #2268bc;"></i> R</li>-->
+        <!--<li><i class="devicon-sass-original colored"></i> Sass</li>-->
+        <!--<li><i class="fab fa-affiliatetheme"></i> Flask</li>-->
+        <!--<li><i class="devicon-react-original colored"></i> React</li>-->
+        <!--<li><i class="devicon-rails-plain colored"></i> Ruby On Rails</li>-->
+        <!--<li><i class="devicon-docker-plain colored"></i> Docker</li>-->
+        <!--<li><i class="devicon-android-plain colored"></i> Android Studio</li>-->
+        <!--<li><i class="devicon-visualstudio-plain" style="color: #21a0eb;"></i> VS Code</li>-->
+        <!--<li><i class="devicon-vim-plain colored"></i> Vim</li>-->
+        <!--<li><i class="devicon-intellij-plain colored"></i> IntelliJ</li>-->
+        <!--<li><i class="devicon-gulp-plain colored"></i> Gulp</li>-->
+        <!--<li><i class="devicon-yarn-plain colored"></i> Yarn</li>-->
+        <!--<li><i class="devicon-postgresql-plain colored"></i> PostgreSQL</li>-->
+        <!-- <li>Jupyter Notebooks</li>
+        <li>Pandas</li>
+        <li>Numpy</li>
+        <li>Matplotlib</li>
+        <li>Seaborn</li>
+        <li>Scikit-Learn</li>
+        <li>Servlet</li>
+        <li>JSP</li>
+        <li>Spring</li>
+        <li>JDBC</li>
+        <li>JUnit</li>
+        <li>Data Science</li>
+        <li>Machine Learning</li>
+        <li>System Administration</li>
+        <li>Web Development</li> -->
+      </ul>
+    </section>
+    <!-- End #skills -->
+
+    <footer>
+      <?php include("./common/footer.php"); ?>
+    </footer>
+    <!-- End footer -->
+
+    <!-- Fixes for Firefox and IE -->
+    <script>
+      var isIE = !!window.MSInputMethodContext && !!document.documentMode;
+      var isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+
+      if (isIE || isFirefox) {
+        var font = document.createElement("link");
+        font.rel = "stylesheet";
+        font.type = "text/css";
+        font.href = "https://fonts.googleapis.com/css?family=Great+Vibes|Lato:300,400,700,900&display=swap";
+        document.head.appendChild(font);
+        var fontAwesome = document.createElement("link");
+        fontAwesome.rel = "stylesheet";
+        fontAwesome.type = "text/css";
+        fontAwesome.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css";
+        document.head.appendChild(fontAwesome);
+        var devicon = document.createElement("link");
+        devicon.rel = "stylesheet";
+        devicon.type = "text/css";
+        devicon.href = "https://cdnjs.cloudflare.com/ajax/libs/devicon/2.4/devicon.min.css";
+        document.head.append(devicon);
+        var bootstrap = document.createElement("link");
+        bootstrap.rel = "stylesstylesheet";
+        bootstrap.type = "text/css";
+        bootstrap.href = "https://cdnjs.cloudflare.com/ajax/libs/devicon/2.4/devicon.min.css";
+        document.head.append(bootstrap);
+      }
+
+      var me=document.getElementById("me");
+      var rot=360;
+      me.onclick=function(){
+      	me.style='transform: rotate('+rot+'deg)';
+      	rot+=360;
+      }
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./scripts/scripts.min.js"></script>
+  </body>
+</html>
