e095010aa2320d2d1dcebf4a750d4594ad8a743d
Ghazali <44301432+gshacklebolt@users.noreply.github.com>
2024-09-07 17:01:00 +0530

    Updated domain link

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,7 @@
         <p id="copyright">
           <script>document.getElementById("copyright").innerHTML="Copyright &copy; "+new Date().getFullYear()+" | All Rights Reserved";</script>
         </p>
-  		  <p>Designed &amp; Developed by <a href="https://www.ghazali.cf/" onMouseOver="this.style.color='#800080'" onMouseOut="this.style.color='#1688f0'">Ghazali</a>
+  		  <p>Designed &amp; Developed by <a href="https://ghazali.co.in/" onMouseOver="this.style.color='#800080'" onMouseOut="this.style.color='#1688f0'">Ghazali</a>
         </p>
         <p class="hiddenText">
           Also checkout <a href="https://www.kasif.tech/">Md Kasif's</a> site
